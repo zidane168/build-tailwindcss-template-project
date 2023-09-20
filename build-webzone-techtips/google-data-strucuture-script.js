6de00c405fc237649527627819697cdeebb332f8
Vi Ly <vi.lh@>
2023-09-20 15:14:14 +0700

    add new file

---
+++ build-webzone-techtips/google-data-strucuture-script.js
@@ -0,0 +1,138 @@
+<script type='application/ld+json'>
+[{
+      "@context": "https://schema.org",
+      "@type": "Article",
+      "headline": "Webzone all things tech tips for web development, web development tips and tricks, HTML, CSS, NextJS, Zustand, CMS, CakePHP, RabbitMQ, SQL Server, MongoDB",
+      "image": [
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGOJ0NSNNM70ZQud6CsoUXWMjTIL_NujZrAOdT0LvrCpeHK1NULuihufyZVAMuIrNhpPVgYXV5pOvtSTX5aveO3bA-uiQPx80spMqREWrsfDJhH1hw1dKVxtJwEq8FulF_Uau3QJHf_BtwR6Xs3Q4rKMC-lO4GTv6ciD_9nY-n2kjlCawG8Oc2BhRqELA/s16000/Migrate%20from%20legacy%20FCM%20APIs%20to%20HTTP%20v1%20-%20Firebase.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiom0JosPNA8BuKBL9hv1M4B8tNDVBMrtKNa-muVAo_5nWjqN-jK66adzWuPAPNTj6pGBn3ko2_eRrbUNU8bbvAG_XoEqBkcf8HH3XsuYgRRNYcPuXZKImiA8YqHfpFQIdtwIhkTGpp_nZ8iAxOLCwkdTWmhRB53STRE-T_aW2xA60VFsj3n1Le3mseZrY/s16000/trigger%20insert%20updated%20deleted.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi56ps0aZHENuFhGWIdFBp0sgzhRmoqI65F-z15rts9aWxEcDK72SQmHdpJgWvKiEkFspoxYAi5fDtx-bqEhZcNJZCQCfyOAOjRx6otdWWid6MbShvc3WeW6mILps6tqSs4BDoLaKnRK13rCKqXC2_NriPCvw6kywNSDv-EGzMUcZr6TiE3Hz679mYUTaQ/s16000/RabbitMQ%20vs%20Kafka.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiY30CkdurKC1fiW9d-LFkTFCZYp5zBTy6op8ycUf9K80k8d0Klw9Fx5l4FlSugPoeKX8FLB0UIsB81F54vkM6qh9jnVn6AQUD1FbP6njq0AdvbP_KcrWdTkGhfiaDFvlt86UCKyTeYWm6B_1uyEMbt-rz5saqwBXUz32SalZ3Inc1cY3NRAOtyFw-Nczk/s16000/how%20to%20edit%20image%20like%20a%20photoshop%20pro%20using%20CSS.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjp6ZIRsFm44s_xbpKaNAdPR4TCHfZz2K8A9GE4hZAHxqwuCym7MU0RCGKqxO44jB9_573UsYhD8PiwhLGqcABexQ6PR2Vv2BYN1oW7PpyH-pRPhDbivtE0xq-jGwlVtDbO7sfe2jqdFUmhHwGd21n0P9_3lFW5YtH26o84wCKcmeMVAeNBCeHA9IKEQLA/s16000/5%20Best%20Practices%20for%20Managing%20External%20Interactions%20in%20Database%20Objects.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfeVyPoqkXxqrV7GDzvzOHvQkAe_zzfszIWmLl1VO8B46vg_JLW3wO1i5j_pVOu1297plzA2OCyMvnw-lPyk96ldZtI3_AhITI8-EzMMec19-YXfsIEITnN2tc6Ujf9sRptsBj9Vtbv90WyueUCpXlJ_HSKdeBq_-uaAgpeK3pVYXlT_dHQCuwofYQbhg/s16000/Screenshot%202023-07-31%20at%2015.56.56.png",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjj59b21tZuxiRe0zyydeKk8WOzW0W-ra2e-GwUL6wnm5RbjbbeyZ664oXOpxzKzLCD9YZ5OOxCj3efgi-tDmkOgI_JWupKNs_n3GOMqHviIb6LskBob0pYoHFCmBYz5-ahex-RefwVaUiYp_xpas5Yi_oig_Fh6gaX26n_KmjRH7ZXj2Z09L2lqD1slF8/s16000/Thumbnail%20Slider%20Login%20Register%20Form%20gif.gif",
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgehBJHgCxm-f0SwytMsr0JASnr4oN4hC6idiJ0I4_MWyC1YpMqIfQFbIHOOq2kkPM_i0zHkf9pKu-N67f-QsZ5_klwlS1pJMsxvujyvGXUDj7HsjBfdxAr5145-kfnZ_kWK0P-zGy6jI1etEkH2V1UEBYB0x6GEI9dtSXNR1sSDBMqeLz_OgBOGXOKfTo/s16000/%5BTips%5D%20Create%20a%20Stunning%20Login%20Register%20Slider%20Form%20with%20full%20source%20code%20(6).gif"
+      ],
+      "url": "https://learn-tech-tips.blogspot.com/",
+      "datePublished": "2023-08-30T08:00:00+07:00",
+      "dateModified": "2023-09-02T08:00:00+07:00",
+      "author": {
+          "@type": "Person",
+          "name": "Vi Ly (Zidane)",
+          "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        } 
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "WHAT SHOULD I USE RABBITMQ VS KAFKA",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+        "name": "WHAT SHOULD I USE RABBITMQ VS KAFKA",
+        "description": "Deciding between RabbitMQ and Kafka depends on your specific requirements and use case. Here are some factors to consider when choosing between the two: 1. Use case, 2. Scalability",
+          "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi56ps0aZHENuFhGWIdFBp0sgzhRmoqI65F-z15rts9aWxEcDK72SQmHdpJgWvKiEkFspoxYAi5fDtx-bqEhZcNJZCQCfyOAOjRx6otdWWid6MbShvc3WeW6mILps6tqSs4BDoLaKnRK13rCKqXC2_NriPCvw6kywNSDv-EGzMUcZr6TiE3Hz679mYUTaQ/s16000/RabbitMQ%20vs%20Kafka.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/08/what-should-i-use-rabbitmq-vs-kafka.html" 
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "HOW TO MONITOR DATA CHANGES IN SQL SERVER TABLES WITH TRIGGERS",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+         "name": "HOW TO MONITOR DATA CHANGES IN SQL SERVER TABLES WITH TRIGGERS", 
+          "description": "SQL Server is a powerful and popular relational database management system that supports various data operations. However, sometimes you may want to keep track of the changes made to your data, such as when, who, and what was inserted, updated, or deleted in a table. This can be useful for auditing, debugging, or reporting purposes.One way to achieve this is by using triggers, which are special stored procedures that execute automatically in response to certain events. In this article, we will show you how to create and use triggers to monitor data changes in SQL Server tables. There are three types of triggers in SQL Server: DML, DDL, and Logon triggers. DML triggers fire when an INSERT, UPDATE, or DELETE statement affects a table or view. DDL triggers fire when a CREATE, ALTER, or DROP statement affects a database object. Logon triggers fire when a user logs on to the database.",
+          "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiom0JosPNA8BuKBL9hv1M4B8tNDVBMrtKNa-muVAo_5nWjqN-jK66adzWuPAPNTj6pGBn3ko2_eRrbUNU8bbvAG_XoEqBkcf8HH3XsuYgRRNYcPuXZKImiA8YqHfpFQIdtwIhkTGpp_nZ8iAxOLCwkdTWmhRB53STRE-T_aW2xA60VFsj3n1Le3mseZrY/s1600/trigger%20insert%20updated%20deleted.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2015/06/%20how-to-monitor-data-changes-in-sql-server-tables-with-triggers.html"       
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "CAKEPHP PLUGIN FOR USAGE OF RABBITMQ",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+         "name": "CAKEPHP PLUGIN FOR USAGE OF RABBITMQ", 
+          "description": "CakePHP is a PHP web application framework that can be used to build web applications that interact with RabbitMQ. Here's an full tutorial detail guideline for how to use CakePHP with RabbitMQ",
+          "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfeVyPoqkXxqrV7GDzvzOHvQkAe_zzfszIWmLl1VO8B46vg_JLW3wO1i5j_pVOu1297plzA2OCyMvnw-lPyk96ldZtI3_AhITI8-EzMMec19-YXfsIEITnN2tc6Ujf9sRptsBj9Vtbv90WyueUCpXlJ_HSKdeBq_-uaAgpeK3pVYXlT_dHQCuwofYQbhg/s1662/Screenshot%202023-07-31%20at%2015.56.56.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/08/cakephp-plugin-for-usage-of-rabbitmq.html"        
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "HOW TO BUILD A BEAUTIFUL SLIDER LOGIN REGISTER FORM IN 5 MINUTES",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+        "name": "HOW TO BUILD A BEAUTIFUL SLIDER LOGIN REGISTER FORM IN 5 MINUTES", 
+        "description": "If you're looking for a way to spice up your website or app with a slider login register form, you've come to the right place. In this post, I will share with you how to create this awesome feature in just 5 minutes. You don't need any advanced skills or tools to follow along with me. Let's get started. In this tutorial, you will learn how to build a beautiful HTML CSS slider login register in 5 minutes. A slider login register is a user interface element that allows users to switch between login and register forms by sliding the screen horizontally. This can create a smooth and elegant user experience for your website or application. You will use HTML to create the structure of the slider, CSS to style and animate the slider, and JavaScript to add interactivity and functionality to the slider. By following this tutorial, you will be able to create a responsive and modern slider login register that works on different devices and screen sizes.",
+        "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjj59b21tZuxiRe0zyydeKk8WOzW0W-ra2e-GwUL6wnm5RbjbbeyZ664oXOpxzKzLCD9YZ5OOxCj3efgi-tDmkOgI_JWupKNs_n3GOMqHviIb6LskBob0pYoHFCmBYz5-ahex-RefwVaUiYp_xpas5Yi_oig_Fh6gaX26n_KmjRH7ZXj2Z09L2lqD1slF8/s1920/Thumbnail%20Slider%20Login%20Register%20Form%20gif.gif"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/06/how-to-build-a-beautiful-slider-login-register-form-in-5-minutes.html"
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "WHAT IS ELASTICSEARCH AND HOW DOES IT WORK?",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+        "name": "WHAT IS ELASTICSEARCH AND HOW DOES IT WORK?", 
+        "description": "Elasticsearch is a free and open-source tool that lets you search and analyze data quickly and easily. It is based on Apache Lucene, which is a library for working with text data. Elasticsearch can do many things, such as: ... ",
+        "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEirpXLVpJwfZHuOlzhz9crFp15P2Fco8g57TlCszaDQf-q6xghLYAXI8W0Fbf_1A9Yq0QIgoScinxqbkawRYlNDdW_b9MhOFR9voAQo3XOLUTJha7qIpH1jqwXAme_Z3d1fuJE9iXDyWwh7EAtw-_VwnMZNKzip18jPSHhKEZl9enmYPtiziIgsUvvr94U/s788/What%20is%20Elasticsearch%20and%20How%20Does%20It%20Work.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/09/what-is-elasticsearch-and-how-does-it-work.html"
+    },
+    { 
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "AUTHENTICATOR APPS: HOW TO PROTECT YOUR ONLINE ACCOUNTS WITH ONE TIME PASSWORDS",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+        "name": "AUTHENTICATOR APPS: HOW TO PROTECT YOUR ONLINE ACCOUNTS WITH ONE TIME PASSWORDS", 
+        "description": "An authentication app, also known as a two-factor authentication (2FA) app or TOTP (Time-Based One-Time Password) app, is a mobile application that provides an additional layer of security for user authentication. It is designed to enhance the security of online accounts, applications, and services by requiring users to enter a time-sensitive verification code generated by the app along with their regular username and password.",
+        "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhW2b9fm7m71Xf-yLoOzkcbqknbCBQ3Lo_qCy-sEh78Q86cVtLIINM1pUbpW9qGK7X0Z37foZygronK485qVu-tZdEJP7S72CltA9uUYQRQP1KbSiBVCuGDL1Pomp2EFKhV5z40IeFkKZ07GeuBoRfoVCu23zGKB2nBzKxHg2WEM8SK5FaW8U9CQMsgc-w/s849/Authenticator%20app.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/08/authenticator-apps-how-to-protect-your-online-accounts-with-one-time-passwords.html"
+    },
+    {
+        "@context": "https://schema.org/", 
+        "@type": "Article",
+        "headline": "HOW TO USE DOCKER COMPOSE TO SET UP A RABBITMQ SERVICE",
+        "author": {
+            "@type": "Person",
+            "name": "Vi Ly (Zidane)",
+            "url": "https://learn-tech-tips.blogspot.com/p/about_83.html"
+        }, 
+        "name": "HOW TO USE DOCKER COMPOSE TO SET UP A RABBITMQ SERVICE", 
+        "description": "RabbitMQ is a popular open-source message broker that allows applications to communicate with each other asynchronously. It supports various protocols and platforms, and can be easily scaled and integrated with other services.",
+        "image": [
+            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjDsleaS2FCsypdozl61L4JK6VuvLfcigSnAwiOiwjZ0H4hSo5Xppgy5wj5SxrktAOonLrBkrSk-6ak_LSJR2j9hndblXXw8wmXYGFIlevxrG2gNN9EqtespyDfXOCwoO4boom5kzsVYMnWc5SzhOvbOhS2ndd_tugjPd2laCJ1UjdsiJilhezJcet-QBs/s796/RabbitMQ%20Login%20page.png"
+        ],
+        "url": "https://learn-tech-tips.blogspot.com/2023/07/how-to-use-docker-compose-to-set-up-a-rabbitmq-service.html"
+    } 
+]
+</script>
